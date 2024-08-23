b02d44986af6a2ba67d973e3a3d527cab3867d4a
wankhadesarvesh1997 <wankhadesarvesh1997@gmail.com>
2024-08-23 13:06:37 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -1,4 +1,5 @@
 #vim .bashrc
+#fi
 #export PATH=$PATH:/usr/local/bin/
 #source .bashrc
 
@@ -11,8 +12,8 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket sarveshwankhade007.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket sarveshwankhade007.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
+aws s3api create-bucket --bucket sarveshwankhade007.k8s.local --region ap-south-1
+aws s3api put-bucket-versioning --bucket sarveshwankhade007.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://sarveshwankhade007.k8s.local
-kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
-kops update cluster --name rahams.k8s.local --yes --admin
+kops create cluster --name sarvesh.k8s.local --zones ap-south-1 --master-count=1 --master-size t2.micro --node-count=2 --node-size t2.micro
+kops update cluster --name sarvesh.k8s.local --yes --admin
